4bf24c68a3c1b6186b70d300ecc9e9049caecd6c
Haseebm01 <haseebdr01@gmail.com>
2024-02-25 20:52:34 -0800

    Installed depends

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from "react";
-// import { ReactDOM } from "react";
-// import { render } from "react-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from './App';
 import './global.css';
-import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+document.getElementById('root')
+);
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="root" />);
+
+//     <createRoot>
+//         <App />
+//     </createRoot>,
+// document.getElementById('root')
+
+// function AppWithCallbackAfterRender() {
+    // useEffect(() => {
+    //   console.log('rendered');
+    // });
+  
+    // return <App tab="home" />
+//   }
+//   const container = document.getElementById('app');
+//   const root = createRoot(container);
+//   root.render(<AppWithCallbackAfterRender />);
 
