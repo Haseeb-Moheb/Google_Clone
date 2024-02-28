614538f5a6451780e1cb8bc5374d17fd32eb5517
Haseebm01 <haseebdr01@gmail.com>
2024-02-27 22:49:46 -0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { Routes } from './components/Routes';
